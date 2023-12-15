9d6a246c81d700d7a7dc389dac2eb70a1c8e2aab
Oliver Juss <oliverjuss007@gmail.com>
2023-12-15 21:30:54 +0200

    add week 16 code

---
+++ Week16BitCoinApp/BitCoinRate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16BitCoinApp
+{
+    public class BitCoinRate
+    {
+        public bpi bpi {  get; set; }
+    }
+
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+    }
+
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public string rate_float { get; set; }
+    }
+
+
+}
+
---
+++ Week16BitCoinApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using Week16BitCoinApp;
+
+BitCoinRate bitcoin = GetRates();
+
+Console.WriteLine($"Current Bitcoin rate is: {bitcoin.bpi.EUR.rate_float} {bitcoin.bpi.EUR.code} ");
+
+static BitCoinRate GetRates()
+{
+    string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "GET";
+
+    var webResponse = request.GetResponse();
+    var webStream = webResponse.GetResponseStream();
+
+    BitCoinRate bitcoinRate;
+
+    using(var responseReader = new StreamReader(webStream))
+    {
+        var response = responseReader.ReadToEnd();
+        bitcoinRate = JsonConvert.DeserializeObject<BitCoinRate>(response);
+    }
+
+    return bitcoinRate;
+}
